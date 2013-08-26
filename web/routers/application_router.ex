16fb42dfaf33735e884415350630693823f31bef
John Warwick <jwarwick@gmail.com>
2013-08-25 21:52:33 -0400

    Added layouts and partials

--- web/routers/application_router.ex
+++ web/routers/application_router.ex
@@ -5,7 +5,8 @@ defmodule ApplicationRouter do
     # Pick which parts of the request you want to fetch
     # You can comment the line below if you don't need
     # any of them or move them to a forwarded router
-    conn.fetch([:cookies, :params])
+    conn = conn.fetch([:cookies, :params])
+    conn = conn.assign(:layout, "layout")
   end
 
   # It is common to break your Dynamo in many
@@ -13,14 +14,17 @@ defmodule ApplicationRouter do
   # forward "/posts", to: PostsRouter
 
   get "/" do
-    conn = conn.assign(:title, "Welcome to Dynamo!")
     render conn, "index.html"
+  end
+
+  get "/thanks" do
+    render conn, "thanks.html"
   end
 
   post "/data" do
     IO.inspect conn.params
     IO.puts "First name: #{conn.params[:firstname]}"
-    redirect conn, to: "/"
+    redirect conn, to: "/thanks"
   end
 
 end
